71c9e0365a4730b2e57662d0168c714e539c9c95
Maksim Kostromin <daggerok@gmail.com>
2017-08-31 06:25:20 +0300

    Fix template

--- 0-template/deployment/postdeploy.bash
+++ 0-template/deployment/postdeploy.bash
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # replace base-path with root:
-# /startbootstrap-freelancer/ -> /
+# /pwa-examples/ -> /
 
 if ! [ -z "$1" ]; then
-  sed -i -e "s/\<head\>\<base\ href=\"\/startbootstrap-freelancer\/\"\/>/\<head\>/g" $1
+  sed -i -e "s/\<head\>\<base\ href=\"\/pwa-examples\/\"\/>/\<head\>/g" $1
+  sed -i -e "s/\(href=\"\/pwa-examples\/\)/href=\"\//g" $1
+  sed -i -e "s/\(src=\"\/pwa-examples\/\)/src=\"\//g" $1
   rm -rf .nojekyll 404.html index.html-e
 fi
--- 0-template/deployment/predeploy.bash
+++ 0-template/deployment/predeploy.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # add base tag:
-# <head> -> <head><base href="/startbootstrap-freelancer/"/>
+# <head> -> <head><base href="/pwa-examples/"/>
 
 if ! [ -z "$1" ]; then
-  sed -i -e "s/\(<head>\)/<head><base href=\"\/startbootstrap-freelancer\/\"\/>/g" $1
+  sed -i -e "s/\(src=\"\/\)/src=\"\/pwa-examples\//g" $1
+  sed -i -e "s/\(href=\"\/\)/href=\"\/pwa-examples\//g" $1
+  sed -i -e "s/\(<head>\)/<head><base href=\"\/pwa-examples\/\"\/>/g" $1
 fi
