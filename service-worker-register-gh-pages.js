934072a86d8ed4f2c50fda82538d78e4a3e671ad
Maksim Kostromin <daggerok@gmail.com>
2017-09-06 22:24:12 +0300

    Github Pages 2017-09-06 deployment

--- service-worker-register-gh-pages.js
+++
@@ -1,1 +0,0 @@
-w([1],{8:function(e,r){navigator.serviceWorker.controller?console.log("[PWA Builder] active service worker found, no need to register"):navigator.serviceWorker.register("service-worker.js",{scope:"./"}).then(function(e){console.log("Service worker has been registered for scope:"+e.scope)})}},[8]);
