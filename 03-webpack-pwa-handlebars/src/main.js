4528ed09864dfbad837a7dcaed3f654328336b3b
Maksim Kostromin <daggerok@gmail.com>
2017-09-07 08:14:45 +0300

    Moving parts: (using handlebars templates)

---
+++ 03-webpack-pwa-handlebars/src/main.js
@@ -0,0 +1,7 @@
+import appTemplate from './components/App.hbs';
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('#app').innerHTML = appTemplate({
+    title: 'hi!'
+  });
+});
